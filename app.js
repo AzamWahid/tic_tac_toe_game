7bd6c01349e849b352bf53c7110adef5d2b4d9a9
azam.wahid <azamwahid@outlook.com>
2024-10-25 01:46:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('welcome to tic tac toe');
+
+let music = new Audio('./assets/music.mp3');
+let turnMusic = new Audio('./assets/ting.mp3');
+let gameOver = new Audio('./assets/gameover.mp3');
+
+
+let turn  = "X";
+
+// fucntion to change the turn
+const changeTurn = ()=>{
+
+    return turn === "X"?"0" : "X";
+}
+
+
+
+//fucntion to check win
+
+const checkWin =() => {
+
+}
+
+
+//game logic
+
